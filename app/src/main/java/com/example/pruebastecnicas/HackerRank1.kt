0011af5c0fc7b7b69b09d98a3aa43745998a370d
krtapia <hishrcontact4@gmail.com>
2022-11-11 12:35:10 -0600

    add  changes
    add changes

--- app/src/main/java/com/example/pruebastecnicas/HackerRank1.kt
+++ app/src/main/java/com/example/pruebastecnicas/HackerRank1.kt
@@ -1,9 +1,21 @@
 package com.example.pruebastecnicas
 
-fun compareTriplets(a: Array<Int>, b: Array<Int>) {
-    if(a==b){
-        var result = true
+fun compareTriplets(a: Array<Int>, b: Array<Int>): Array<Int> {
+    var scoreA = 0
+    var scoreB = 0
+
+    val ArrSize = a.size - 1
+
+    for (item in 0..ArrSize){
+
+        if(a[item] > b[item])
+            scoreA += 1
+
+        if(a[item] < b[item])
+            scoreB += 1
     }
+
+    return arrayOf(scoreA, scoreB)
 }
 
 fun main(args: Array<String>) {
@@ -12,7 +24,7 @@ fun main(args: Array<String>) {
 
     val b = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
 
-    var result = compareTriplets(a, b)
+    val result = compareTriplets(a, b)
 
     println(result.toString())
 }
